43eff509654cc217cf6380039d432f7ae7585736
israelopeters <israelopeters@gmail.com>
2025-05-08 09:12:56 +0100

    feat: Set up app navigation

---
+++ app/src/main/java/com/example/readingnow/ReadingNowApp.kt
@@ -0,0 +1,113 @@
+package com.example.readingnow
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.readingnow.service.UserViewModel
+import com.example.readingnow.ui.components.AppAlertDialog
+import com.example.readingnow.ui.components.AppScreen
+import com.example.readingnow.ui.components.CustomBottomAppBar
+import com.example.readingnow.ui.components.CustomTopAppBar
+import com.example.readingnow.ui.screens.HomeScreen
+import com.example.readingnow.ui.screens.SignUpScreen
+import com.example.readingnow.ui.screens.WelcomeScreen
+
+@Composable
+fun ReadingNowApp(
+    modifier: Modifier = Modifier,
+    userViewModel: UserViewModel = hiltViewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = AppScreen.valueOf(
+        backStackEntry?.destination?.route ?: AppScreen.Welcome.name
+    )
+    var openSignOutAlertDialog by remember { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            val noTopBarScreens = listOf(
+                AppScreen.Welcome.name,
+                AppScreen.SignUp.name,
+            )
+            if (!noTopBarScreens.contains(currentScreen.name)) {
+                CustomTopAppBar(
+                    currentScreen = currentScreen,
+                    canNavigate = navController.previousBackStackEntry != null,
+                    navigateUp = {  },
+                    onSignOutClicked = {
+                        openSignOutAlertDialog = true
+                    }
+                )
+            }
+        },
+        bottomBar = {
+            CustomBottomAppBar(
+                onHomeClicked = { navController.navigate(AppScreen.Home.name) },
+                onProfileClicked = { }
+            )
+        },
+        modifier = modifier
+
+    ) { innerPadding ->
+
+        when {
+            openSignOutAlertDialog -> {
+                AppAlertDialog(
+                    onDismissRequest = { openSignOutAlertDialog = false },
+                    onConfirmation = {
+                        openSignOutAlertDialog = false
+                        userViewModel.signOut()
+                        navController.navigate(AppScreen.Welcome.name)
+                    },
+                    dialogTitle = stringResource(R.string.sign_out_confirmation),
+                    dialogText = stringResource(R.string.sign_out_confirmation_text),
+                    icon = Icons.Filled.Info,
+                    modifier = Modifier.padding(innerPadding)
+                )
+            }
+        }
+
+        NavHost(
+            navController = navController,
+            startDestination = AppScreen.Welcome.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = AppScreen.Welcome.name) {
+                WelcomeScreen(
+                    userViewModel = userViewModel,
+                    onContinueClicked = { navController.navigate(AppScreen.Home.name) },
+                    onSignUpClicked = { navController.navigate(AppScreen.SignUp.name) },
+                    modifier = modifier.fillMaxSize()
+                )
+            }
+            composable(route = AppScreen.Home.name) {
+                HomeScreen(userViewModel = userViewModel)
+            }
+            composable(route = AppScreen.SignUp.name) {
+                SignUpScreen(
+                    userViewModel = userViewModel,
+                    onSignInClicked = {
+                        userViewModel.clearAddedUser()
+                        navController.navigate(AppScreen.Welcome.name)
+                    }
+                )
+            }
+        }
+    }
+}
